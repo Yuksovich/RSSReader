e3a4afef14a8f8e59d0985b463a683cabf43bcfb
Yuksovich <yuksiph@gmail.com>
2016-11-04 23:05:17 +0700

    xml styles, rss columns class

--- app/src/main/java/yuriy/rssreader/MainActivity.java
+++ app/src/main/java/yuriy/rssreader/MainActivity.java
@@ -1,15 +1,12 @@
 package yuriy.rssreader;
 
 import android.app.DialogFragment;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.*;
@@ -20,7 +17,7 @@ import yuriy.rssreader.service.RssProviderService;
 import yuriy.rssreader.ui.AddNewUrlDialog;
 import yuriy.rssreader.ui.SingleRssView;
 
-public final class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public final class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private final static String DIALOG_NEW_URL = "dialogNewUrl";
     private static final String KEY_ITEM_LINK = "itemLink";
@@ -47,12 +44,12 @@ public final class MainActivity extends AppCompatActivity implements AdapterView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_actionbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(toolbar);
 
         setContentView(R.layout.activity_main);
         refreshList();
-        final ImageButton refreshButton = (ImageButton)findViewById(R.id.refreshButton_toolbar);
+        final ImageButton refreshButton = (ImageButton) findViewById(R.id.refreshButton_toolbar);
         refreshButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,7 +57,7 @@ public final class MainActivity extends AppCompatActivity implements AdapterView
             }
         });
 
-        final ImageButton addNewUrlButton = (ImageButton)findViewById(R.id.addUrlButton_toolbar);
+        final ImageButton addNewUrlButton = (ImageButton) findViewById(R.id.addUrlButton_toolbar);
         addNewUrlButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,7 +66,7 @@ public final class MainActivity extends AppCompatActivity implements AdapterView
             }
         });
 
-        final ImageButton filterButton = (ImageButton)findViewById(R.id.filterButton_toolbar);
+        final ImageButton filterButton = (ImageButton) findViewById(R.id.filterButton_toolbar);
         filterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
@@ -84,33 +81,35 @@ public final class MainActivity extends AppCompatActivity implements AdapterView
         return true;
     }
 
-    private void refreshList(){
+    private void refreshList() {
         new Thread(new ListViewDataProvider(this, handler)).start();
         listView = (ListView) findViewById(R.id.listOfEntries);
         listView.setFastScrollEnabled(true);
         Intent intent = new Intent(this, RssProviderService.class);
         startService(intent);
-        listView.showContextMenu();
     }
 
     @Override
     public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-        final TextView textView= (TextView)view.findViewById(R.id.item_link);
+        final TextView textView = (TextView) view.findViewById(R.id.item_link);
         final String itemLink = textView.getText().toString();
         Intent intent = new Intent(this, SingleRssView.class);
         intent.putExtra(KEY_ITEM_LINK, itemLink);
         startActivity(intent);
     }
 
-    private void showPopupMenu(View view){
+    private void showPopupMenu(View view) {
         PopupMenu popupMenu = new PopupMenu(this, view);
         popupMenu.inflate(R.menu.filter_popup);
         popupMenu.show();
         Menu menu = popupMenu.getMenu();
         ChannelSelectionPopup channelSelectionPopup = new ChannelSelectionPopup(this, menu);
-
-
-
     }
 
-   }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refreshList();
+
+    }
+}
